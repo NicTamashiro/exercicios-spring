017d1cc86b3ac854c93d6e22572d4353c1c5a542
Nicolas <adilsonq1976@gmail.com>
2026-07-14 19:02:42 -0300

    Exercicio 14 - Streams

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe14/Main.java
@@ -0,0 +1,14 @@
+package br.com.nicolas.exercicios_spring.exe14;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6);
+
+        numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
