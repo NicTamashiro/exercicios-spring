b6aaf7627bf5dd6a1b46777e5da0dc51220e198a
Nicolas <adilsonq1976@gmail.com>
2026-07-14 19:41:23 -0300

    Exercicio 21 - Streams

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe21/Main.java
@@ -0,0 +1,28 @@
+package br.com.nicolas.exercicios_spring.exe21;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Produto> produtos = Arrays.asList(
+                new Produto("Smartphone", 800.0, "Eletrônicos"),
+                new Produto("Notebook", 1500.0, "Eletrônicos"),
+                new Produto("Teclado", 200.0, "Eletrônicos"),
+                new Produto("Cadeira", 300.0, "Móveis"),
+                new Produto("Monitor", 900.0, "Eletrônicos"),
+                new Produto("Mesa", 700.0, "Móveis")
+        );
+
+        List<Produto> produtosFiltrados = produtos.stream()
+                .filter(p -> p.getCategoria().equals("Eletrônicos"))
+                .filter(p -> p.getPreco() < 1000)
+                .sorted(Comparator.comparing(Produto::getPreco))
+                .limit(3)
+                .collect(Collectors.toList());
+
+        System.out.println(produtosFiltrados);
+    }
+}
