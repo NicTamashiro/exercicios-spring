489f97d912c145ac0b72f5b75e07859aaa5d48ea
Nicolas <adilsonq1976@gmail.com>
2026-07-14 19:12:47 -0300

    Exercicio 16 - Streams

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe16/Main.java
@@ -0,0 +1,17 @@
+package br.com.nicolas.exercicios_spring.exe16;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6);
+        List<Integer> resultado = numeros.stream()
+                .filter(n -> n % 2 != 0)
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+
+        System.out.println(resultado);
+    }
+}
