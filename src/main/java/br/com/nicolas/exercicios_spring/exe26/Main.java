476f5b178954ed9ce4f258066069c4ab6cd62e6e
Nicolas <adilsonq1976@gmail.com>
2026-07-15 13:39:29 -0300

    Exercicio 26

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe26/Main.java
@@ -0,0 +1,17 @@
+package br.com.nicolas.exercicios_spring.exe26;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6);
+        Map<Boolean, List<Integer>> partitioned = nums.stream()
+                .collect(Collectors.partitioningBy(n -> n % 2 == 0));
+
+        System.out.println("Even: " + partitioned.get(true));
+        System.out.println("Odd: " + partitioned.get(false));
+    }
+}
