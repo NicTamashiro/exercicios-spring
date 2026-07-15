c977af461246f1fae1eadf63a9f7881bf59d71e5
Nicolas <adilsonq1976@gmail.com>
2026-07-15 13:53:49 -0300

    Exercicio 27

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe27/Main.java
@@ -0,0 +1,25 @@
+package br.com.nicolas.exercicios_spring.exe27;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Product> produtos = Arrays.asList(
+                new Product("Smartphone", 800.0, "Eletrônicos"),
+                new Product("Notebook", 1500.0, "Eletrônicos"),
+                new Product("Teclado", 200.0, "Eletrônicos"),
+                new Product("Cadeira", 300.0, "Móveis"),
+                new Product("Monitor", 900.0, "Eletrônicos"),
+                new Product("Mesa", 700.0, "Móveis")
+        );
+
+        Map<String, List<Product>> grouped = produtos.stream()
+                .collect(Collectors.groupingBy(Product::getCategoria));
+
+        System.out.println(grouped);
+
+    }
+}
