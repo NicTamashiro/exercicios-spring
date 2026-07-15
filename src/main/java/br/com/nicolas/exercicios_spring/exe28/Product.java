4317dea99261f7bb1717853e35df066130312177
Nicolas <adilsonq1976@gmail.com>
2026-07-15 13:57:04 -0300

    Exercicio 28

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe28/Product.java
@@ -0,0 +1,34 @@
+package br.com.nicolas.exercicios_spring.exe28;
+
+public class Product {
+    private String nome;
+    private double preco;
+    private String categoria;
+
+    public Product(String nome, double preco, String categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", categoria='" + categoria + '\'' +
+                '}';
+    }
+}
