7c3bcfc3c80d22ea1e2a8b5af87f696b547c7058
Nicolas <adilsonq1976@gmail.com>
2026-07-17 19:19:22 -0300

    Exercicio 36

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe36/Main.java
@@ -0,0 +1,8 @@
+package br.com.nicolas.exercicios_spring.exe36;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Mes.FEVEREIRO.getNumeroDeDias());
+        System.out.println(Mes.JULHO.getNumeroDeDias());
+    }
+}
