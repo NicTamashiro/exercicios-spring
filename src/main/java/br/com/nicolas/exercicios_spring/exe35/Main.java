db5b6cc0c70f809713010053a6383d17f7fc7088
Nicolas <adilsonq1976@gmail.com>
2026-07-17 19:11:20 -0300

    Exercicio 35

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe35/Main.java
@@ -0,0 +1,19 @@
+package br.com.nicolas.exercicios_spring.exe35;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> emails = Arrays.asList("TESTE@EXEMPLO.COM", "exemplo@Java.com ", "Usuario@teste.Com");
+        System.out.println(converterEmails(emails));
+
+    }
+
+    public static List<String> converterEmails(List<String> emails) {
+        return emails.stream()
+                .map(String::toLowerCase)
+                .toList();
+    }
+}
