0e297c5d5daab2f718466dfcaa5151718bb2bb73
Nicolas <adilsonq1976@gmail.com>
2026-07-17 19:07:25 -0300

    Exercicio 34

---
+++ src/main/java/br/com/nicolas/exercicios_spring/exe34/Main.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.exercicios_spring.exe34;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ehPalindromo("socorram me subi no onibus em marrocos")); // Saída: true
+        System.out.println(ehPalindromo("Java")); // Saída: false
+
+    }
+    public static boolean ehPalindromo(String palavra) {
+        String semEspacos = palavra.replace(" ", "").toLowerCase();
+        return new StringBuilder(semEspacos).reverse().toString().equalsIgnoreCase(semEspacos);
+    }
+}
